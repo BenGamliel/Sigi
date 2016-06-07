687d0a57177b44c19e63b07f986425c9a88b911a
lironsoffer <soffer.liron@gmail.com>
2016-06-07 21:36:45 +0300

    Notes

--- Talk.h
+++ Talk.h
@@ -7,18 +7,45 @@
 
 #ifndef TALK_H_
 #define TALK_H_
-
+/*
+ * This Module is responsible for answering the user about Sigi's Mood.
+ *
+ * Talk(const size_t commandIndex, const size_t &calcultionCount,
+ * const string &name) -The Constructor
+ *
+ * bool execute(size_t &printIndex,std::vector<string> &input) -
+ * Responsible to the execution of the Talk command.
+ */
 #include "SigiCommand.h"
 
 class Talk : public SigiCommand {
 public:
+/*
+ * Talk(const size_t commandIndex, const size_t &calcultionCount,
+ * const string &name) -The Constructor
+ *
+ * @param commandIndex - sets the numeric index of the command
+ * @param calcultionCount - a reference to the variable in SigiMapper
+ * which holds the count of the calculations.
+ * @param name - a string reference to the user's name variable
+ */
 	Talk(const size_t commandIndex, const size_t &calcultionCount,
 			const string &name) : SigiCommand(commandIndex),
 			_calcultionCount(calcultionCount), _name(name){}
 
 	virtual ~Talk(){}
 
-		// Functions
+/*
+ * bool execute(size_t &printIndex,std::vector<string> &input) -
+ * Responsible to the execution of the Talk command.
+ *
+ * @param printIndex - Then function updates this reference variable to hold the
+ *  numeric index of the relevant print string in case of error.
+ * Will not change it in case of success.
+ * @param input - A vector which holds only the command in index 0.
+ *
+ * @ return - true for success, false for error.
+ */
 	bool execute(size_t &printIndex, std::vector<string> &input);
 
 private:
@@ -34,6 +61,8 @@ private:
 		// Functions
 	const size_t checkMood() const;
 
+		// Constructors that shall not be used
+	Talk() : SigiCommand(0),_calcultionCount(*new size_t), _name(*new string){}
 
 };
 
