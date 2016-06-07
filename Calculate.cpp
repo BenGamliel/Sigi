3f49faca6a2fccba6c38a2fee43c86c88ff6a9b1
lironsoffer <soffer.liron@gmail.com>
2016-06-07 21:40:04 +0300

    +Fixed all known bugs. +Notes. +Split execute to smaller functions to fit 30 lines limit.

--- Calculate.cpp
+++ Calculate.cpp
@@ -7,41 +7,43 @@
 
 #include "Calculate.h"
 
-
-Calculate::Calculate(const size_t commandIndex, size_t &calcultionCount,string const &name):
-	SigiCommand(commandIndex),_calcultionCount(calcultionCount), _name(name){}
-
 bool Calculate::execute(size_t &printIndex,std::vector<string> &input)
 {
-	bool flag=false;
-	int sign;
-	int numA=atoi((input[1].c_str()));
-	int numB=atoi((input[3].c_str()));
-	size_t foundplus = input[2].rfind("+");
-	size_t foundminus = input[2].rfind("-");
-	if((numA<0)||(numB<0))
+	if((input[1].empty())||(input[3].empty())||(input[2].size()!=1)||input.size()!=4)
 	{
 		printIndex=0;
 		return false;
 	}
-	 if ( (foundminus!=std::string::npos) || (foundplus!=std::string::npos) )
-	 {
-		 flag=true;
-		if(foundplus!=std::string::npos)
-		{
-			sign=1;
-		}
-		else
-			sign=-1;
-	 }
-	if(flag)
+
+	int numA=atoi((input[1].c_str()));
+	int numB=atoi((input[3].c_str()));
+
+	std::cout<<"A= "<< numA << std::endl;
+	std::cout<<"B= "<< numB << std::endl;
+
+
+	int sign = getSign(input[2]);
+	if(sign!=0)
 	{
-		int result=add(numA,numB,sign);
 		_calcultionCount++;
-		std::cout<<_name<<", the result is "<< result << std::endl;
+		std::cout<<_name<<", the result is "<< add(numA,numB,sign) << std::endl;
 		return true;
 	}
+
 	printIndex=0;
 	return false;
 }
 
+int Calculate::getSign(const string &stringSign)
+{
+	if(stringSign=="+")
+	{
+		return 1;
+	}
+	else if (stringSign=="-")
+	{
+		return -1;
+	}
+
+	return false;
+}
--- Calculate.h
+++ Calculate.h
@@ -15,25 +15,26 @@
 #ifndef CALCULATE_H_
 #define CALCULATE_H_
 /*
- * Calculate(const size_t commandIndex,size_t *calcultionCount) -
- * The Constructor
+ * Calculate(const size_t commandIndex,size_t &calcultionCount,
+ * string const &name) - The Constructor
  *
  * @param commandIndex - sets the numeric index of the command
  * @param calcultionCount - a pointer to the variable in SigiMapper
  * which holds the count of the calculations
+ * @param name - a string reference to the user's name variable
  *
- */
-/*
+ *
  *
  * bool execute(size_t &commandIndex,size_t &printIndex) -
  * Responsible to the execution of the calculation.
  *
- * @param commandIndex - The function updates this variable to hold
- * the numeric index of the command.
- * @param printIndex - Then function updates this variable to hold the numeric
- * index of the relevant print string both for success and error.
+ * @param printIndex - Then function updates this reference variable to hold the
+ *  numeric index of the relevant print string in case of error.
+ * Will not change it in case of success.
+ * @param input - holds the relevant input for the command
  *
  * @ return - true for success, false for error.
+ *
  */
 
 #include "SigiCommand.h"
@@ -47,17 +48,23 @@ using namespace std;
 class Calculate : public SigiCommand {
 public:
 
-	Calculate(const size_t commandIndex,size_t &calcultionCount,string const &name);
+	Calculate(const size_t commandIndex,size_t &calcultionCount,
+			string const &name): SigiCommand(commandIndex),
+			_calcultionCount(calcultionCount), _name(name){}
 	virtual ~Calculate(){}
 	bool execute(size_t &commandIndex,std::vector<string> &input);
-	inline int add(const unsigned int numA,const unsigned int numB,const int factor) const
-	{
-		return numA+(factor*numB);
-	}
 
 private:
 	size_t &_calcultionCount;
 	const string &_name ;
+	inline int add(const unsigned int numA,const unsigned int numB,
+			const int factor) const
+	{
+		return numA+(factor*numB);
+	}
+	int getSign(const string &stringSign);
+	Calculate(): SigiCommand(0), _calcultionCount(*new size_t),
+			_name(*new string){}
 };
 
 #endif /* CALCULATE_H_ */
