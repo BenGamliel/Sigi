d5053829f764d3632f9b0d437189e80b6984c3d2
lironsoffer <soffer.liron@gmail.com>
2016-06-07 21:37:35 +0300

    Notes

--- SigiCommand.h
+++ SigiCommand.h
@@ -8,6 +8,14 @@
 #ifndef SIGICOMMAND_H_
 #define SIGICOMMAND_H_
 
+/*
+ * SigiCommand(size_t commandIndex):_commandIndex(commandIndex) -
+ * The Constructor
+ *
+ * virtual bool execute(size_t &printIndex,std::vector<string> &input) -
+ * A Pure Virtual function which is responsible to the execution of the command.
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -15,9 +23,25 @@ using std::string;
 
 class SigiCommand {
 public:
+/*
+ * SigiCommand(size_t commandIndex):_commandIndex(commandIndex) -
+ * The Constructor
+ *
+ * @param commandIndex - sets the numeric index of the command
+ */
 	SigiCommand(size_t commandIndex):_commandIndex(commandIndex){}
 	virtual ~SigiCommand(){}
-
+/*
+ *  virtual bool execute(size_t &printIndex,std::vector<string> &input) -
+ * A Pure Virtual function which is responsible to the execution of the command.
+ *
+ * @param printIndex - Then function updates this reference variable to hold the
+ *  numeric index of the relevant print string in case of error.
+ * Will not change it in case of success.
+ * @param input - A vector which holds the relevant input for the command
+ *
+ * @ return - true for success, false for error.
+ */
 	virtual bool execute(size_t &printIndex,std::vector<string> &input)=0;
 
 protected:
@@ -25,8 +49,6 @@ protected:
 
 private:
 	const size_t _commandIndex;
-
-//	SigiCommand():_commandIndex(0),_name(""){} // TODO: Make it work
 };
 
 #endif /* SIGICOMMAND_H_ */
