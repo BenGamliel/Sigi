8c20b64468db9b65c6b011e01f514bcf102fb63b
lironsoffer <soffer.liron@gmail.com>
2016-06-09 12:37:55 +0300

    Final notes

--- Sing.h
+++ Sing.h
@@ -7,16 +7,45 @@
 
 #ifndef SING_H_
 #define SING_H_
-
+/*
+ * This Module is responsible for the Sing command.
+ *
+ * Sing(const size_t commandIndex, const size_t &calcultionCount) -
+ * The Constructor
+ *
+ * bool execute(size_t &commandIndex,std::vector<string> &input) -
+ * Responsible for Singing.
+ *
+ */
 #include "SigiCommand.h"
 
 class Sing : public SigiCommand {
 public:
+/*
+* Sing(const size_t commandIndex, const size_t &calcultionCount) -
+* The Constructor
+*
+* @param commandIndex - sets the numeric index of the command
+* @param calcultionCount - a reference to the variable in SigiMapper
+* which holds the count of the calculations
+*/
 	Sing(const size_t commandIndex, const size_t &calcultionCount) :
 		SigiCommand(commandIndex),_calcultionCount(calcultionCount){}
 	virtual ~Sing(){}
 
-		// Functions
+/*
+ * bool execute(size_t &commandIndex,std::vector<string> &input) -
+ * Responsible for Singing.
+ *
+ * @param printIndex - Then function updates this reference variable to hold the
+ *  numeric index of the relevant print string in case of error.
+ * Will not change it in case of success.
+ * @param input - A vector which holds the command in index 0
+ * and the relevant input for the command, (the song's name) in index 1.
+ *
+ * @ return - true for success, false for error.
+ *
+ */
 	bool execute(size_t &printIndex, std::vector<string> &input);
 
 private:
@@ -31,6 +60,9 @@ private:
 
 		// Functions
 	size_t getSong(const string &name) const;
+
+		// Constructors that shall not be used
+	Sing() : SigiCommand(0),_calcultionCount(*new size_t){}
 };
 
 #endif /* SING_H_ */
